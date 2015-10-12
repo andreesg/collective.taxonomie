3707515237654618b811d78682ee3f9fe91f7280
André Gonçalves <andreslb1@gmail.com>
2015-10-12 17:03:08 +0200

    ADd support for taxonomic rank

--- collective/taxonomie/utils/indexers.py
+++ collective/taxonomie/utils/indexers.py
@@ -5,57 +5,11 @@ from plone.indexer.decorator import indexer
 from ..taxonomie import ITaxonomie
 
 @indexer(ITaxonomie)
-def nameInformation_addressDetails_place(object, **kw):
+def taxonomicTermDetails_term_scientificName(object, **kw):
     try:
-        terms = []
-        if hasattr(object, 'nameInformation_addressDetails'): 
-            items = object.nameInformation_addressDetails
-            if items:
-                for item in items:
-                    if item['place']:
-                        for term in item['place']:
-                            if term:
-                                terms.append(term)
-
-        if hasattr(object, 'personDetails_birthDetails_place'):
-            items = object.personDetails_birthDetails_place
-            if items:
-                for item in items:
-                    terms.append(item)
-
-        if hasattr(object, 'personDetails_deathDetails_place'):
-            items = object.personDetails_deathDetails_place
-            if items:
-                for item in items:
-                    terms.append(item)
-        
-        if hasattr(object, 'personDetails_placeOfActivity'): 
-            items = object.personDetails_placeOfActivity
-            if items:
-                for item in items:
-                    if item['place']:
-                        for term in item['place']:
-                            if term:
-                                terms.append(term)
-        return terms
-    except:
-        return []
-
-@indexer(ITaxonomie)
-def nameInformation_addressDetails_country(object, **kw):
-    try:
-        if hasattr(object, 'nameInformation_addressDetails'):
-            terms = []
-            items = object.nameInformation_addressDetails
-            if items:
-                for item in items:
-                    if item['country']:
-                        for term in item['country']:
-                            if term:
-                                terms.append(term)
-
-            return terms
+        if hasattr(object, 'taxonomicTermDetails_term_scientificName'):
+            return object.taxonomicTermDetails_term_scientificName
         else:
-            return []
+            return ""
     except:
-        return []
+        return ""
