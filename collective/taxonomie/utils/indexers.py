95cabf9c7f7d52cd62fc5760942977211f7facce
André Gonçalves <andreslb1@gmail.com>
2015-10-12 17:08:16 +0200

    hotfix

--- collective/taxonomie/utils/indexers.py
+++ collective/taxonomie/utils/indexers.py
@@ -5,10 +5,10 @@ from plone.indexer.decorator import indexer
 from ..taxonomie import ITaxonomie
 
 @indexer(ITaxonomie)
-def taxonomicTermDetails_term_scientificName(object, **kw):
+def taxonomicTermDetails_term_rank(object, **kw):
     try:
-        if hasattr(object, 'taxonomicTermDetails_term_scientificName'):
-            return object.taxonomicTermDetails_term_scientificName
+        if hasattr(object, 'taxonomicTermDetails_term_rank'):
+            return object.taxonomicTermDetails_term_rank
         else:
             return ""
     except:
